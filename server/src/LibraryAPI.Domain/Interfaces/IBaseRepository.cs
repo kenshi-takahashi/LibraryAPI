4e886bcfc138cd347821f32ac3b5e039a9ee9cc2
Kenshi-Takahashi <suslikgory12345555@gmail.com>
2024-08-25 05:13:41 +0300

    fix: update IBookRepository change the id data type to int in the IBaseRepository

--- server/src/LibraryAPI.Domain/Interfaces/IBaseRepository.cs
+++ server/src/LibraryAPI.Domain/Interfaces/IBaseRepository.cs
@@ -1,7 +1,7 @@
 public interface IBaseRepository<T> where T : class
 {
     Task<IEnumerable<T>> GetAllAsync();
-    Task<T> GetByIdAsync(Guid id);
+    Task<T> GetByIdAsync(int id);
     Task AddAsync(T entity);
     Task UpdateAsync(T entity);
     Task DeleteAsync(T entity);
