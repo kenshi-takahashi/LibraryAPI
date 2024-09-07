7d417b1be456a7fac443d55810ce4175894ad6ce
Kenshi-Takahashi <suslikgory12345555@gmail.com>
2024-09-07 19:50:37 +0300

    add autoMapper and MappingProfile

---
+++ server/src/LibraryAPI.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,30 @@
+using AutoMapper;
+using LibraryAPI.Domain.Entities;
+using LibraryAPI.Application.DTOs;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // Author mappings
+        CreateMap<AuthorRequestDto, Author>();
+        CreateMap<Author, AuthorResponseDto>();
+
+        // Book mappings
+        CreateMap<BookCreateRequestDto, Book>();
+        CreateMap<BookUpdateRequestDto, Book>();
+        CreateMap<Book, BookAdminResponseDto>();
+        CreateMap<Book, BookUserResponseDto>();
+
+        // User mappings
+        CreateMap<UserLoginRequestDto, User>()
+            .ForMember(dest => dest.Password, opt => opt.Ignore());
+        CreateMap<UserRegistrationRequestDto, User>()
+            .ForMember(dest => dest.Password, opt => opt.Ignore());
+        CreateMap<UserSelfUpdateRequestDto, User>()
+            .ForMember(dest => dest.Password, opt => opt.Ignore());
+        CreateMap<UserAdminUpdateRequestDto, User>();
+        CreateMap<User, UserAdminResponseDto>();
+        CreateMap<User, UserSelfResponseDto>();
+    }
+}
