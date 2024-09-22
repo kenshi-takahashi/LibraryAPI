260eed5821192027d940a4822e0fd26063d6082a
kenshi-takahashi <suslikgory12345555@gmail.com>
2024-09-22 23:33:36 +0300

    feat: add implementation for IAuthorRepository

--- server/src/LibraryAPI.Infrastructure/Repositories/AuthorRepository.cs
+++ server/src/LibraryAPI.Infrastructure/Repositories/AuthorRepository.cs
@@ -1,6 +1,14 @@
 using LibraryAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 public class AuthorRepository : BaseRepository<Author>, IAuthorRepository
 {
     public AuthorRepository(LibraryAPIDbContext context) : base(context) { }
+
+    public async Task<IEnumerable<Book>> GetBooksByAuthorIdAsync(int authorId)
+    {
+        return await _context.Books
+            .Where(b => b.AuthorId == authorId)
+            .ToListAsync();
+    }
 }
