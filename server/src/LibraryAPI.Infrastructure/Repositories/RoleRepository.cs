326aecefb47b34ba45b526159f52beddb91df0ba
kenshi-takahashi <suslikgory12345555@gmail.com>
2024-09-22 23:29:15 +0300

    fix: now use mapper in AuthService now Role finding by the Name, not Id in the Register method in the AuthService

--- server/src/LibraryAPI.Infrastructure/Repositories/RoleRepository.cs
+++ server/src/LibraryAPI.Infrastructure/Repositories/RoleRepository.cs
@@ -1,6 +1,12 @@
 using LibraryAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 public class RoleRepository : BaseRepository<Role>, IRoleRepository
 {
     public RoleRepository(LibraryAPIDbContext context) : base(context) { }
+
+    public async Task<Role> GetByNameAsync(string roleName)
+    {
+        return await _dbSet.FirstOrDefaultAsync(r => r.Name == roleName);
+    }
 }
