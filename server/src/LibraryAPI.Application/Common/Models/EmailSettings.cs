349063a7ab8be8c6ff8f0d57e9c6bc670539b0f1
kenshi-takahashi <suslikgory12345555@gmail.com>
2024-09-23 22:25:11 +0300

    feat: add EmailSettings, google_client_secret.json, GmailServiceInitializer update appsettings.json (add EmailSettings)

---
+++ server/src/LibraryAPI.Application/Common/Models/EmailSettings.cs
@@ -0,0 +1,11 @@
+namespace LibraryAPI.Application.Common
+{
+    public class EmailSettings
+    {
+        public string FromAddress { get; set; }
+        public string SmtpHost { get; set; }
+        public int SmtpPort { get; set; }
+        public string SmtpUser { get; set; }
+        public string SmtpPass { get; set; }
+    }
+}
---
+++ server/src/LibraryAPI.Infrastructure/Services/GmailServiceInitializer.cs
@@ -0,0 +1,43 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Newtonsoft.Json.Linq;
+
+public class GmailServiceInitializer
+{
+    private static string[] Scopes = { GmailService.Scope.GmailSend };
+    private static string ApplicationName = "LibraryAPI";
+
+    public static async Task<GmailService> GetGmailServiceAsync()
+    {
+        UserCredential credential;
+
+        var clientSecretPath = "../LibraryAPI.Infrastructure/Configuration/google_client_secret.json";
+        var json = await File.ReadAllTextAsync(clientSecretPath);
+        var secrets = JObject.Parse(json)["web"];
+
+        var clientSecrets = new ClientSecrets
+        {
+            ClientId = secrets["client_id"].ToString(),
+            ClientSecret = secrets["client_secret"].ToString()
+        };
+
+        var tokenPath = "../LibraryAPI.Infrastructure/Configuration/token.json";
+        using (var stream = new FileStream(tokenPath, FileMode.OpenOrCreate, FileAccess.Read))
+        {
+            credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                clientSecrets,
+                Scopes,
+                "user",
+                CancellationToken.None,
+                new FileDataStore("token.json", true));
+        }
+
+        return new GmailService(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = ApplicationName,
+        });
+    }
+}
